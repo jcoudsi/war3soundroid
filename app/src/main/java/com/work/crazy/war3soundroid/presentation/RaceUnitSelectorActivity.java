0862fabee6bc764ed734470d7754f694f5507b93
Julien Coudsi <jcoudsi@gmail.com>
2015-06-16 20:02:51 +0200

    Initialisation du titre de l'activité de sélection des unités d'une race

--- app/src/main/java/com/work/crazy/war3soundroid/presentation/RaceUnitSelectorActivity.java
+++ app/src/main/java/com/work/crazy/war3soundroid/presentation/RaceUnitSelectorActivity.java
@@ -1,19 +1,45 @@
 package com.work.crazy.war3soundroid.presentation;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.work.crazy.war3soundroid.R;
+import com.work.crazy.war3soundroid.model.RaceEnum;
+import com.work.crazy.war3soundroid.util.Common;
 
 
 public class RaceUnitSelectorActivity extends ActionBarActivity {
+
+    private RaceEnum raceEnum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_race_unit_selector);
+
+        Intent i = getIntent();
+        RaceEnum raceEnum = (RaceEnum)i.getSerializableExtra(Common.launchRaceUnitSelectorActivityRaceKey);
+
+        setTitle(this.getActivityTitle(raceEnum));
+    }
+
+    private String getActivityTitle(RaceEnum raceEnum) {
+
+        switch (raceEnum) {
+            case Human:
+                return "Humains";
+            case Orc:
+                return "Orcs";
+            case Undead:
+                return "Morts-vivants";
+            case Elf:
+                return "Elfes";
+            default:
+                return "";
+        }
     }
 
 }
--- app/src/main/java/com/work/crazy/war3soundroid/util/Common.java
+++ app/src/main/java/com/work/crazy/war3soundroid/util/Common.java
@@ -1,9 +1,6 @@
 package com.work.crazy.war3soundroid.util;
 
-/**
- * Created by Apex on 16/06/2015.
- */
 public class Common {
 
-    public static String launchRaceUnitSelectorActivityRaceKey = "race";
+    public static String launchRaceUnitSelectorActivityRaceKey = "raceKey";
 }
